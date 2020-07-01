5df61002fcd97d59ee15dfa261afcbc5e3e871f1
227Mahaman <zalkiabass.456@gmail.com>
2020-07-01 01:07:36 +0100

    Recuperation des données Notebooks via l'api

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Notebook} from "./model/notebook";
 
 @Component({
   selector: 'app-notes',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  notebooks: Notebook[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAllNotebooks();
+    //this.getAllNotes();
   }
 
+  public getAllNotebooks(){
+    let url = "http://localhost:8081/api/notebooks/all";
+    this.http.get<Notebook[]>(url).subscribe(
+      res => {
+        this.notebooks = res;
+      },
+      err => {
+        alert("An error has occured;");
+      }
+    );
+  }
 }
